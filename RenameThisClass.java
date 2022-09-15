eb6f7f0e1d8bc0b807ef528af7cd9681fd400152
Colin Miller <colin.miller@mrjaffesclass.com>
2022-09-15 11:34:17 -0700

    Initial commit

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -8,6 +8,8 @@
 public class RenameThisClass
 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int i;
+        float f = 2.5f;
+        boolean flag = true;
     }
 }
